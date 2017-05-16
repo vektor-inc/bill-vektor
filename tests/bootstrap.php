f874107fd685f5b1ab5af63c3ca096f6c0033e08
kurudrive <kurudrive@gmail.com>
2017-05-16 21:41:23 +0900

    [ CI ] テーマチェック用に書き換え

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,24 +2,18 @@
 /**
  * PHPUnit bootstrap file
  *
- * @package Twitter_Shortcode
+ * @package Bill_Vektor
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
-
-// Give access to tests_add_filter() function.
+$_tests_dir = getenv('WP_TESTS_DIR');
+if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 require_once $_tests_dir . '/includes/functions.php';
 
-/**
- * Manually load the plugin being tested.
- */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/twitter-shortcode.php';
+	register_theme_directory( dirname( __FILE__ ) . '/../../' );
+	switch_theme('bill-vektor');
 }
+
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
