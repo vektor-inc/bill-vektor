7111db75f6c66c7234177fb5e1a1c3f802d9f95b
catherine <catherine5th.avenue@gmail.com>
2017-10-05 17:52:00 +0900

    testしたよー！taxのとこね！

--- tests/test-prices.php
+++ tests/test-prices.php
@@ -47,8 +47,7 @@ class PriceTest extends WP_UnitTestCase {
 
 			// 価格を取得
 			$number = bill_item_number( $test_value['number_input'] );
-
-			// 
+			//
 			$this->assertEquals( $test_value['number_correct'], $number );
 
 			print PHP_EOL;
@@ -63,7 +62,7 @@ class PriceTest extends WP_UnitTestCase {
 	function test_price() {
 
 		$test_array = array(
-			// 
+			//
 			array(
 				'item_count' => 2,
 				'item_price' => 400,
@@ -71,8 +70,8 @@ class PriceTest extends WP_UnitTestCase {
 			),
 			// 価格の四捨五入
 			array(
-				'item_count' => 2.75,
-				'item_price' => 5,
+				'item_count' => '2.７５',
+				'item_price' => '５',
 				'item_price_correct' => 14,
 			),
 			);
@@ -80,14 +79,61 @@ class PriceTest extends WP_UnitTestCase {
 		foreach ( $test_array as $key => $test_value) {
 
 			// 価格を取得
-			$item_price_total = bill_item_price_total( $test_value['item_count'], $test_value['item_price'] );
-
-			// 
+			$item_price_total = bill_item_price_total( bill_item_number( $test_value['item_count'] ), bill_item_number( $test_value['item_price'] ) );
+			//
 			$this->assertEquals( $test_value['item_price_correct'], $item_price_total );
 
 			print PHP_EOL;
 			print 'item_price            :'.$item_price_total.PHP_EOL;
 			print 'item_price_correct    :'.$test_value['item_price_correct'].PHP_EOL;
-		}
-	}
+		} // foreach ( $test_array as $key => $test_value) {
+	} // function test_price() {
+
+
+	/**
+	 * 消費税の計算が正しいかどうか？
+	 */
+	function test_tax() {
+
+		$test_array = array(
+				// 消費税の端数が 0.4 以下だった場合
+				array(
+					'price' => 92,
+					'tax_correct' => 7,
+				),
+				// 消費税の端数が 0.5 以上だった場合
+				array(
+					'price' => 98,
+					'tax_correct' => 7,
+				),
+			);
+
+		foreach ( $test_array as $key => $test_value) {
+
+			// 価格を取得
+			$tax = bill_tax( $test_value[ 'price' ] );
+			//
+			$this->assertEquals( $test_value['tax_correct'], $tax );
+
+			print PHP_EOL;
+			print 'price:'.$tax.PHP_EOL;
+			print 'tax_correct:'.$test_value['tax_correct'].PHP_EOL;
+		} // foreach ( $test_array as $key => $test_value) {
+	} // function test_price() {
+
+
+	// 消費税が正しく計算されるかどうか
+	// function test_bill_total_add_tax() {
+	// 	$test_array = array(
+	// 		// 消費税を計算した時の端数が0.4以下のとき
+	// 		array(
+	//
+	// 		),
+	// 		// 消費税を計算した時の端数が0.5以下のとき
+	// 		array(
+	//
+	// 		),
+	// 		);
+	// }
+
 }
