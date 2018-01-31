e45e1d544cfe6448d870166d9fc7015425be4191
kurudrive <kurudrive@gmail.com>
2018-01-31 18:21:27 +0900

    [ 仕様変更 ] 詳細ページで未ログインだった場合、ログイン後トップページにリダイレクトするのではなくログイン前にアクセスしたページを表示するように変更

--- inc/functions-limit-view.php
+++ inc/functions-limit-view.php
@@ -1,35 +1,41 @@
 <?php
-/*-------------------------------------------*/
-/*  No login redirect
-/*-------------------------------------------*/
-/*  wp_head _ add noindex, nofollow
-/*-------------------------------------------*/
+/*
+---------------------------------------------
+  No login redirect
+---------------------------------------------
+  wp_head _ add noindex, nofollow
+---------------------------------------------
+ */
 
 
-/*-------------------------------------------*/
-/*  No login redirect
-/*-------------------------------------------*/
+ /*
+ ---------------------------------------------
+   No login redirect
+ ---------------------------------------------
+ */
 function bill_no_login_redirect( $content ) {
-  global $pagenow;
-  if( !is_user_logged_in() && !is_admin() ){
-	// ログインページへリダイレクト
-	$redirectUrl = home_url();
-	$url = wp_login_url($redirectUrl);
-	header("Location: {$url}");
-	exit;
-    
-    /*
-    auth_redirect() の場合、アドレスバーにURLをhttps無しで直入力された時に、ログイン先は http でも実際にはhttpsなので認証が通らなくて無限ループになる
-    */
+	global $pagenow;
+	if ( ! is_user_logged_in() && ! is_admin() ) {
+		// ログインページへリダイレクト
+		$redirectUrl = home_url( $_SERVER['REQUEST_URI'] );
+		$url         = wp_login_url( $redirectUrl );
+		header( "Location: {$url}" );
+		exit;
 
-  }
-}//bill_no_login_redirect
-add_action( 'get_header', 'bill_no_login_redirect' );
+		/*
+		auth_redirect() の場合、アドレスバーにURLをhttps無しで直入力された時に、ログイン先は http でも実際にはhttpsなので認証が通らなくて無限ループになる
+		*/
 
-/*-------------------------------------------*/
-/*  wp_head _ add noindex, nofollow
-/*-------------------------------------------*/
-function bill_add_nofollow(){
-  echo '<meta name="robots" content="noindex, nofollow">';
+	}
+}//end bill_no_login_redirect()
+add_action( 'wp', 'bill_no_login_redirect' );
+
+/*
+---------------------------------------------
+	wp_head _ add noindex, nofollow
+---------------------------------------------
+*/
+function bill_add_nofollow() {
+	echo '<meta name="robots" content="noindex, nofollow">';
 }
 add_action( 'wp_head', 'bill_add_nofollow' );
