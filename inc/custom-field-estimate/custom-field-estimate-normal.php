bacd64447061114e4f538beadccc4ecb8a553cac
kurudrive <kurudrive@gmail.com>
2018-04-09 12:08:02 +0900

    [ cf-bulider ] PHPCS （整形のみ）

--- inc/custom-field-estimate/custom-field-estimate-normal.php
+++ inc/custom-field-estimate/custom-field-estimate-normal.php
@@ -5,28 +5,26 @@
 
 class Estimate_Normal_Custom_Fields {
 	public static function init() {
-		add_action( 'admin_menu' , array( __CLASS__, 'add_metabox'), 10, 2);
-		add_action( 'save_post' , array( __CLASS__, 'save_custom_fields'), 10, 2);
+		add_action( 'admin_menu', array( __CLASS__, 'add_metabox' ), 10, 2 );
+		add_action( 'save_post', array( __CLASS__, 'save_custom_fields' ), 10, 2 );
 	}
 
 	// add meta_box
-	public static function add_metabox() 
-	{
+	public static function add_metabox() {
 
-		$id = 'meta_box_bill_normal';
-		$title = __( '見積書項目', '' );
-		$callback = array( __CLASS__, 'fields_form');
-		$screen = 'estimate';
-		$context = 'advanced';
-		$priority = 'high';
+		$id            = 'meta_box_bill_normal';
+		$title         = __( '見積書項目', '' );
+		$callback      = array( __CLASS__, 'fields_form' );
+		$screen        = 'estimate';
+		$context       = 'advanced';
+		$priority      = 'high';
 		$callback_args = '';
 
 		add_meta_box( $id, $title, $callback, $screen, $context, $priority, $callback_args );
 
 	}
 
-	public static function fields_form()
-	{
+	public static function fields_form() {
 		global $post;
 
 		$custom_fields_array = Estimate_Normal_Custom_Fields::custom_fields_array();
@@ -34,102 +32,97 @@ class Estimate_Normal_Custom_Fields {
 		VK_Custom_Field_Builder::form_table( $custom_fields_array, $befor_custom_fields );
 	}
 
-	public static function save_custom_fields()
-	{
+	public static function save_custom_fields() {
 		$custom_fields_array = Estimate_Normal_Custom_Fields::custom_fields_array();
 		// $custom_fields_array_no_cf_builder = arra();
 		// $custom_fields_all_array = array_merge(  $custom_fields_array, $custom_fields_array_no_cf_builder );
 		VK_Custom_Field_Builder::save_cf_value( $custom_fields_array );
 	}
 
-	public static function custom_fields_array()
-	{
+	public static function custom_fields_array() {
 
 		$args = array(
-			'post_type' => 'client',
+			'post_type'      => 'client',
 			'posts_per_page' => -1,
-			'order' => 'ASC',
-			'orderby' => 'title',
-			);
+			'order'          => 'ASC',
+			'orderby'        => 'title',
+		);
 
-		$client_posts = get_posts($args);
+		$client_posts = get_posts( $args );
 		if ( $client_posts ) {
-			$client = array( "" => "選択してください");
-			foreach ($client_posts as $key => $post) {
+			$client = array( '' => '選択してください' );
+			foreach ( $client_posts as $key => $post ) {
 				// プルダウンに表示するかしないかの情報を取得
-				$client_hidden = get_post_meta(  $post->ID, 'client_hidden', true );
+				$client_hidden = get_post_meta( $post->ID, 'client_hidden', true );
 				// プルダウン非表示にチェックが入っていない項目だけ出力
-				if ( ! $client_hidden )  {
-						$client[$post->ID] = $post->post_title;
+				if ( ! $client_hidden ) {
+						$client[ $post->ID ] = $post->post_title;
 				}
-			}	
+			}
 		} else {
-			$client = array( "0" => "請求先が登録されていません");
+			$client = array( '0' => '請求先が登録されていません' );
 		}
 
 		$custom_fields_array = array(
-			'bill_client' => array(
-				'label' => __('取引先','bill-vektor'),
-				'type' => 'select',
-				'description' => '取引先は<a href="'.admin_url('/post-new.php?post_type=client').'" target="_blank">こちら</a>から登録してください。',
-				'required' => true,
-				'options' => $client,
+			'bill_client'              => array(
+				'label'       => __( '取引先', 'bill-vektor' ),
+				'type'        => 'select',
+				'description' => '取引先は<a href="' . admin_url( '/post-new.php?post_type=client' ) . '" target="_blank">こちら</a>から登録してください。',
+				'required'    => true,
+				'options'     => $client,
 			),
 			// 'bill_id' => array(
-			// 	'label' => __('受け渡し期間','bill-vektor'),
-			// 	'type' => 'text',
-			// 	'description' => '',
-			// 	'required' => false,
+			// 'label' => __('受け渡し期間','bill-vektor'),
+			// 'type' => 'text',
+			// 'description' => '',
+			// 'required' => false,
 			// ),
 			// 'bill_Issue_date' => array(
-			// 	'label' => __('発行日','bill-vektor'),
-			// 	'type' => 'datepicker',
-			// 	'description' => '',
-			// 	'required' => true,
+			// 'label' => __('発行日','bill-vektor'),
+			// 'type' => 'datepicker',
+			// 'description' => '',
+			// 'required' => true,
 			// ),
-			'bill_tax_type' => array(
-				'label' => __('消費税','bill-vektor'),
-				'type' => 'radio',
+			'bill_tax_type'            => array(
+				'label'       => __( '消費税', 'bill-vektor' ),
+				'type'        => 'radio',
 				'description' => '',
-				'required' => false,
-				'options' => array( 
-					'tax_auto' => '最後にまとめて自動計算する（デフォルト）',
+				'required'    => false,
+				'options'     => array(
+					'tax_auto'     => '最後にまとめて自動計算する（デフォルト）',
 					'tax_not_auto' => '品目毎に予め消費税込の金額で入力する',
-					),
+				),
 			),
 			'bill_total_price_display' => array(
-				'label' => __('合計の表示','bill-vektor'),
-				'type' => 'checkbox',
+				'label'       => __( '合計の表示', 'bill-vektor' ),
+				'type'        => 'checkbox',
 				'description' => '価格の目安リストなど、金額の合計を表示しない場合はチェックを入れてください。',
-				'required' => false,
-				'options' => array( 
+				'required'    => false,
+				'options'     => array(
 					'hidden' => '合計金額を表示しない',
-					),
+				),
 			),
-			'bill_remarks' => array(
-				'label' => __('備考','bill-vektor'),
-				'type' => 'textarea',
-				'description' => '共通の備考は<a href="'.menu_page_url( 'bill-setting-page', false ).'" target="_blank">'.'請求設定画面</a>から設定してください。<br>こちらの備考が記入されている場合は共通の備考は表示されません。',
-				'required' => false,
+			'bill_remarks'             => array(
+				'label'       => __( '備考', 'bill-vektor' ),
+				'type'        => 'textarea',
+				'description' => '共通の備考は<a href="' . menu_page_url( 'bill-setting-page', false ) . '" target="_blank">' . '請求設定画面</a>から設定してください。<br>こちらの備考が記入されている場合は共通の備考は表示されません。',
+				'required'    => false,
 			),
-			'bill_memo' => array(
-				'label' => __('メモ','bill-vektor'),
-				'type' => 'textarea',
+			'bill_memo'                => array(
+				'label'       => __( 'メモ', 'bill-vektor' ),
+				'type'        => 'textarea',
 				'description' => 'この項目は見積書には印刷されません。',
-				'required' => false,
+				'required'    => false,
 			),
-		// 'event_image_main' => array(
-		//     'label' => __('メインイメージ','bill-vektor'),
-		//     'type' => 'image',
-		//     'description' => '',
-		//     'hidden' => true,
-		//     ),
+			'bill_send_pdf'            => array(
+				'label'       => __( '送付PDF', 'bill-vektor' ),
+				'type'        => 'file',
+				'description' => '',
+				'hidden'      => true,
+			),
 		);
 		return $custom_fields_array;
 	}
 
 }
 Estimate_Normal_Custom_Fields::init();
-
-
-
