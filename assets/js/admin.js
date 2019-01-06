e25987ae3380e3d71654da157e3899401aa08a68
kurudrive <kurudrive@gmail.com>
2019-01-06 12:37:29 +0900

    [ 仕様変更 ] 行を複製した時に、複製元の内容が扶養な事の方が多いので空で複製されるように変更

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -4,6 +4,14 @@
 	jQuery('.row-control .add-row').click(function(){
 		var $master_html = '';
 		jQuery(this).closest('tr').clone(true).insertAfter(jQuery(this).closest('tr'));
+
+		// 複製した行の値を消す
+		jQuery(this).closest('tr').next().find('td').each(function(){
+			jQuery(this).find('input[type=text]').attr({'value':''});
+			jQuery(this).find('option').removeAttr('selected');
+			jQuery(this).find('textarea').text('');
+		});
+		
 		row_count_reset();
 	});
 
@@ -21,6 +29,8 @@
 				// 置換対象の文字列
 				var input_name = jQuery(this).attr("name");
 
+				// []内が数字にマッチしている部分のみ置換する。
+				// 数字の前部分が $1 、数字の部分が今のループ回数 、後部分が $3
 				var result = input_name.replace(/(.*\[)(\d+)(\].*)/, "$1" + i + "$3" );
 				jQuery(this).attr({"name":result}).attr({"id":result});
 
@@ -33,7 +43,7 @@
 
 	jQuery('#sortable').sortable();
 	jQuery('#sortable').disableSelection();
-	    
+
 	jQuery('#sortable').bind('sortstop', function (e, ui) {
 	    // ソートが完了したら実行される。
 
