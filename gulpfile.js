dcf68cc6bc6b49f73538d922b58212d53008a837
Asahi Himura <himura@nitolab.com>
2020-02-10 17:24:31 +0900

    gulpfileの修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,26 @@
 'use strict';
 
 var gulp = require('gulp');
-var runSequence = require('run-sequence'); // 同期的に処理してくれる
 
-gulp.task('copy', function() {
-    return gulp.src(
-            [
-                './**/*.png',
-                './**/*.jpg',
-                './**/*.gif',
-                './**/*.php',
-                './assets/**',
-                './inc/**',
-                './template-parts/**',
-                './readme.md',
-                './style.css',
-                "!./tests/**",
-                "!./dist/**",
-                "!./node_modules/**/*.*"
-            ],
-            { base: './' }
-        )
-        .pipe( gulp.dest( 'dist' ) ); // distディレクトリに出力
+gulp.task('copy', (done) => {
+    gulp.src(
+        [
+            './**/*.png',
+            './**/*.jpg',
+            './**/*.gif',
+            './**/*.php',
+            './assets/**',
+            './inc/**',
+            './template-parts/**',
+            './readme.md',
+            './style.css',
+            "!./tests/**",
+            "!./dist/**",
+            "!./node_modules/**/*.*"
+        ],
+        {
+            base: './'
+        }
+    )
+        .pipe(gulp.dest( 'dist' ))
 } );
-
-gulp.task('build:dist',function(){
-    /* ここで、CSS とか JS をコンパイルする */
-});
-
-gulp.task('dist', function(cb){
-    // return runSequence( 'build:dist', 'copy', cb );
-    return runSequence( 'build:dist', 'copy', cb );
-});
