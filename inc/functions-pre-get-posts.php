54c547c3152f0eb8af1f3d45d1bf2a458515104e
kurudrive <kurudrive@gmail.com>
2021-05-14 12:06:29 +0900

    phpcs

--- inc/functions-pre-get-posts.php
+++ inc/functions-pre-get-posts.php
@@ -1,35 +1,39 @@
 <?php
-function bill_custom_home_post_type($query){
-    if ( !is_admin() && !is_singular() && $query->is_main_query() ) {
+function bill_custom_home_post_type( $query ) {
+	if ( ! is_admin() && ! is_singular() && $query->is_main_query() ) {
 
-		$client_id = ( isset( $_GET['bill_client'] ) && $_GET['bill_client'] ) ? esc_attr( $_GET['bill_client'] ) : "";
-		$post_type = ( isset( $_GET['post_type'] ) && $_GET['post_type'] ) ? esc_attr( $_GET['post_type'] ) : "";
+		$client_id = ( isset( $_GET['bill_client'] ) && $_GET['bill_client'] ) ? esc_attr( $_GET['bill_client'] ) : '';
+		$post_type = ( isset( $_GET['post_type'] ) && $_GET['post_type'] ) ? esc_attr( $_GET['post_type'] ) : '';
 
 		$query->set( 'post_type', $post_type );
 		if ( $client_id ) {
-			$meta_query[] = array( 'key'=>'bill_client','value' => $client_id );
+			$meta_query[] = array(
+				'key'   => 'bill_client',
+				'value' => $client_id,
+			);
 			$query->set( 'meta_query', $meta_query );
 		}
 
-		/*	期間の絞り込み
+		/*
+		  期間の絞り込み
 		/*-------------------------------------------*/
 		$start_date = ( isset( $_GET['start_date'] ) && $_GET['start_date'] ) ? $_GET['start_date'] : '';
-		$end_date = ( isset( $_GET['end_date'] ) && $_GET['end_date'] ) ? $_GET['end_date'].' 23:59:59' : '';
+		$end_date   = ( isset( $_GET['end_date'] ) && $_GET['end_date'] ) ? $_GET['end_date'] . ' 23:59:59' : '';
 		// if ( $start_date && $end_date ){
 			// $start_date = $start_date.' 00:00:00';
 			// $end_date = $end_date.' 23:59:59';
 			$date_query = array(
 				array(
-					'compare'=>'BETWEEN',
+					'compare' => 'BETWEEN',
 					// 'inclusive'=>ture,
-					'after'=> $start_date,
-					'before'=> $end_date
-					)
-				);
+					'after'   => $start_date,
+					'before'  => $end_date,
+				),
+			);
 			$query->set( 'date_query', $date_query );
-		// }
+			// }
 
-        return;
-    }
+			return;
+	}
 }
 add_action( 'pre_get_posts', 'bill_custom_home_post_type' );
