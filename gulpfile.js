b6566b4b726edc5bdce85b5679d864f57a2151fe
kurudrive <kurudrive@gmail.com>
2022-06-05 23:12:00 +0900

    dist調整

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 'use strict';
 
 var gulp = require('gulp');
-var runSequence = require('run-sequence'); // 同期的に処理してくれる
 
-gulp.task('copy', function() {
+gulp.task('dist', function() {
     return gulp.src(
             [
                 './**/*.png',
@@ -23,12 +22,3 @@ gulp.task('copy', function() {
         )
         .pipe( gulp.dest( 'dist/bill-vektor' ) ); // distディレクトリに出力
 } );
-
-gulp.task('build:dist',function(){
-    /* ここで、CSS とか JS をコンパイルする */
-});
-
-gulp.task('dist', function(cb){
-    // return runSequence( 'build:dist', 'copy', cb );
-    return runSequence( 'build:dist', 'copy', cb );
-});
